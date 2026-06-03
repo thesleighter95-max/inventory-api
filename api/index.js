c33b7403fa56001180956b0ea24ed796843a1151
thesleighter95-max <thesleighter95@gmail.com>
2026-06-03 11:25:01 +0700

    refactor: remove index.js, switching to catch-all route

--- api/index.js
+++
@@ -1,9 +0,0 @@
-export const runtime = "edge";
-
-export default async function handler(req) {
-  const url = new URL(req.url);
-  const path = url.pathname.replace(/^\/?api/, "") || "/";
-  return new Response(JSON.stringify({ status: "ok", path }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
